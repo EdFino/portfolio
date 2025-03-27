0ef2c75e74ca535ca319909b6337becfa7cf0910
Ed <seibeledouard@yahoo.fr>
2025-03-27 19:11:45 +0100

    First progress on my second component my projects, I hope it will look good

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,15 @@
 import { Component, OnInit } from '@angular/core';
-import { ProjectComponent } from './project/project.component';
-import { ProjectListComponent } from './project-list/project-list.component';
 import { HeaderComponent } from './header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { MyprojectsComponent } from "./myprojects/myprojects.component";
 
 @Component({
   selector: 'app-root',
   imports: [
     HeaderComponent,
-    RouterOutlet,],
+    RouterOutlet,
+    MyprojectsComponent
+],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
